4aea6ae3125ec12ff1f12d52f6c0a3e4b06998fb
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-07-13 19:01:04 +0500

    Initial Commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Main, NextScript } from "next/document";
+import Head from "next/head";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>
+          Twitter Clone!
+        </title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>
+                Twitter Clone
+            </h1>
+        </div>
+    )
+};
+
+export default Home;
