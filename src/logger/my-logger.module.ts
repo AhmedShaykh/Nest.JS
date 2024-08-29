7d2bcc5c3b03caea62545e0e4a099cd7b6fe1bec
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-08-29 15:30:41 +0500

    Final Commit

--- src/logger/my-logger.module.ts
+++
@@ -1,8 +0,0 @@
-import { MyLoggerService } from "./my-logger.service";
-import { Module } from "@nestjs/common";
-
-@Module({
-    providers: [MyLoggerService],
-    exports: [MyLoggerService]
-})
-export class MyLoggerModule { };
