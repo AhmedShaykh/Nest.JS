8c1883beee63d1062db9d8ab08a4a6f1280bca70
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-08-29 15:34:25 +0500

    Final Commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { AllExceptionsFilter } from "./all-exceptions.filter";
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+
+  const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.enableCors();
+
+  app.setGlobalPrefix("api");
+
+  await app.listen(8080);
+
+};
+
+bootstrap();
